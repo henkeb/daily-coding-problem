ce9707b712ef0fe0ec9dd350a3fe07130aae0eb7
Henrik Berg <khp.berg@gmail.com>
2023-06-09 16:49:37 +0200

    Prepare day 11

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,4 +1,5 @@
 pub mod p10_job_scheduler;
+pub mod p11_autocomplete;
 pub mod p12_staircase;
 pub mod p14_monte_carlo;
 pub mod p1_sum_of_numbers_in_array;
---
+++ src/solutions/p11_autocomplete.rs
@@ -0,0 +1,23 @@
+// Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+//
+// For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+//
+// Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
+//
+
+fn autocomplete<'a>(input: Vec<&'a str>, query: &'a str) -> Vec<&'a str> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ok_input() {
+        assert_eq!(
+            autocomplete(vec!["dog", "deer", "deal"], "de"),
+            vec!["deer", "deal"]
+        );
+    }
+}
