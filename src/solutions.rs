c457fa115de84093fc653f7a58d08dd03965cff7
Henrik Berg <khp.berg@gmail.com>
2023-06-09 11:33:01 +0200

    proper mod names

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,8 +1,8 @@
-pub mod decodable_7;
-pub mod find_missing_positive_integer;
-pub mod product_of_elements_in_array;
-pub mod serialize_and_deserialize_tree;
-pub mod staircase;
-pub mod sum_of_numbers_in_array;
-pub mod unival_tree_8;
-pub mod xor_linked_list_6;
+pub mod p12_staircase;
+pub mod p1_sum_of_numbers_in_array;
+pub mod p2_product_of_elements_in_array;
+pub mod p3_serialize_and_deserialize_tree;
+pub mod p4_find_missing_positive_integer;
+pub mod p6_xor_linked_list;
+pub mod p7_decodable;
+pub mod p8_unival_tree;
